6d6619a502a0cd4e14eab3df6d58b515c9a8cf24
nstolfe <nstolfe@gmail.com>
2024-01-23 19:39:33 -0600

    Finalized 'Defeated Dinosaurs' function in the Player class. Replaced the Player 'name' attribute with a 'revival token' attribute to prepare for the 'Player Revive Dinosaur' function.

--- dinoduel.py
+++ dinoduel.py
@@ -10,35 +10,35 @@
 #__init__ all classes DONE
 #Each class needs three attributes: ex: self.name, self.age, self.color:
   #Dinosaur: #1) Attack, #2) Defense, #3) Health DONE
-  #Player: #1) Active Dinosaur #2) Defeated Dinosaur #3) Player Name
+  #Player: #1) Active Dinosaur #2) Defeated Dinosaur #3) Revival Tokens
 #Each class needs three methods: ex: def change_color(self, color):
   #Dinosaur: deal damage (attack), lose health, heal DONE
-  #Player: Name Dinosaur, Revive dinosaur, check active dinosars? check defeated dinosaurs?
+  #Player: Revive dinosaur, check active dinosars, check defeated dinosaurs
 #Each class needs to describe itself with __repr__(self):
 #Each class needs to have at least two instances (or class objects) DONE
   #Dinosaur Class instances: T-rex and Stegosarus
   #Player Class instances: Player 1 and Player 2
 #The two classes need to interact with each other in some way DONE
-  #Name Dinosaur
-
-#Additonal Ideas:
-  #Use a while loop to check dinosaur hp and keep rotating between players to take actions?
+  #Check active/defeated dinosaur
 
 #Left to do:
-    #Create final two player attributes
     #Create final two player methods
     #Describe player class with __repr__
 
+#Allows the text art of the dinosaurs that will be shown in the terminal when players name the dinosaurs.
+    #Uses cowsay.trex() and cowsay.stegosaurus() to designate the T-rex and Stegosaurs art.
+    #Used in the Player class "name_dinosaurs" function.
 import cowsay
 
 class Dinosaur:
-    def __init__(self, attack, defense, health, species = "t-rex"):
+    def __init__(self, attack, defense, health, name, species):
         self.attack = attack
         self.defense = defense
         self.health = health
+        self.name = name
         self.species = species
 
-    #Calculates and updates remaining health of the dinosaur
+    #Calculates and updates remaining health of the dinosaur.
     def lose_health(self, damage_dealt):
         self.health -= damage_dealt
         if self.species == "t-rex":
@@ -47,7 +47,7 @@ class Dinosaur:
             print("After the attack, the Stegosarus has {health} hit points remaining.".format(health = self.health))
         return self.health
 
-    #Calculates hit point loss for the update_health function
+    #Calculates hit point loss for the update_health function.
     def deal_damage(self):
         if self.species == "t-rex":
             damage_dealt = 40
@@ -56,7 +56,7 @@ class Dinosaur:
         #Allows the damage calculated to be passed into the parameter for damage_dealt in the update_health function
         return damage_dealt
     
-    #Allows a non-defeated dinosaur to heal by 100HP
+    #Allows a non-defeated dinosaur to heal by 100HP.
     def heal_self(self):
         if self.species == "t-rex":
             if self.health > 0:
@@ -71,87 +71,251 @@ class Dinosaur:
             else:
                 print("Your Stegosarus is already defeated in battle.")
 
+    #Allows player to check the stats of their dinosaur.
     def __repr__(self):
         return """Your dinosaur is a {species}.\nYour {species} has {health} hit points, {attack} attack power, {defense} defense power.""".format(species = self.species.title(), health = self.health, attack = self.attack, defense = self.defense)
-
  
 class Player(Dinosaur):
-    def __init__(self, active_dinosaurs, defeated_dinosaurs, player = "Player 1"):
+    def __init__(self, active_dinosaurs, defeated_dinosaurs, revival_tokens):
         self.active_dinosaurs = active_dinosaurs
         self.defeated_dinosaurs = defeated_dinosaurs
+        self.revival_tokens = revival_tokens
 
-    def name_dinosaurs(self, Dinosaur, Dinosaur2):
-        if Dinosaur.species == "t-rex":
-            user_dino_name = input("Welcome to Dino Duel, Player 1. Player 1 you are the two T-rexes. Player 1, what would you like the name of your first t-rex to be? ")
+    #Allows the player to see which dinosaurs are still active in battle. Dinosaurs will be referenced by the name the player provided earlier.
+    def check_active_dinosaurs(self, active_dinosaurs, defeated_dinosaurs):
+        #Checking if the dinosaur has health left to be considered active or not.
+        if len(active_dinosaurs) == 0:
+            print("You have no active dinosaurs to duel with.")
         else:
-            user_dino_name = input("Welcome to Dino Duel, Player 2. Player 2 you are the Stegosaruses. Player 2, what would you like the name of your first stegosaurus to be? ")
-        #Accounting for any user capitalization errors to the dinosaur name
-        dino_name = user_dino_name.title()
-
-        if Dinosaur.species == "t-rex":
-            user_dino2_name = input("Player 1, what would you like the name of your second t-rex to be? ")
-        else:
-            user_dino2_name = input("Player 2, what would you like the name of your second stegosaurus to be? ")
-        #Accounting for any user capitalization errors to the dinosaur name
-        dino2_name = user_dino2_name.title()
-        
-        #Returning the names of the dinosaurs to the terminal with the dinosaur text art
-        if Dinosaur.species == "t-rex":
-            print(cowsay.trex("The first T-rex is named " + dino_name))
-            print(cowsay.trex("The second T-rex is named " + dino2_name))
-        else:
-            print(cowsay.stegosaurus("The first Stegosarus is named " + dino_name))
-            print(cowsay.stegosaurus("The second Stegosarus is named " + dino2_name))
-
-    def check_active_dinosaurs(self, active_dinosaurs):
-        # for dinosaur in active_dinosaurs:
-        #     print(dinosaur)
-
-        #use "is defeated == true" identifier?
-        print("Your active dinosaurs are {dinosaur}".format(dinosaur = self.active_dinosaurs))
+            for Dinosaur in active_dinosaurs:
+                if Dinosaur.health > 0:
+                    pass
+                else:
+                    defeated_dinosaurs.append(Dinosaur)
+                    active_dinosaurs.remove(Dinosaur)
+        #Using the active dinosaur list defined when the player object is later created to determine which dinosaur name should be
+        #returned when the active dinosaur list is called.
+        if len(active_dinosaurs) > 0:
+            if Dinosaur.species == "t-rex":
+                if len(active_dinosaurs) == 2:
+                    print("Your active dinosaurs are {dinosaur1} and {dinosaur2}.".format(dinosaur1 = trex_name, dinosaur2 = trex2_name))
+                elif len(active_dinosaurs) == 1:
+                    for Dinosaur in active_dinosaurs:
+                        if Dinosaur.name == trex_name:
+                            print("Your active dinosaur is {dinosaur}.".format(dinosaur = trex_name))
+                        elif Dinosaur.name == trex2_name:
+                            print("Your active dinosaur is {dinosaur}.".format(dinosaur = trex2_name))
+                elif len(active_dinosaurs) == 0:
+                    print("You have no active dinosaurs to duel with.")
+            else:
+                if len(active_dinosaurs) == 2:
+                    print("Your active dinosaurs are {dinosaur1} and {dinosaur2}.".format(dinosaur1 = stego_name, dinosaur2 = stego2_name))
+                elif len(active_dinosaurs) == 1:
+                    for Dinosaur in active_dinosaurs:
+                        if Dinosaur.name == stego_name:
+                            print("Your active dinosaur is {dinosaur}.".format(dinosaur = stego_name))
+                        elif Dinosaur .name == stego2_name:
+                            print("Your active dinosaur is {dinosaur}.".format(dinosaur = stego2_name))
+                elif len(active_dinosaurs) == 0:
+                    print("You have no active dinosaurs to duel with.")
     
+    #Allows the player to see which dinosaurs are still active in battle. Dinosaurs will be referenced by the name the player provided earlier.
+    def check_defeated_dinosaurs(self, defeated_dinosaurs, active_dinosaurs):
+        #Checking if the dinosaur has health left to be considered active or not.
+        if len(defeated_dinosaurs) >= 0:
+            for Dinosaur in active_dinosaurs:
+                if Dinosaur.health <= 0:
+                    defeated_dinosaurs.append(Dinosaur)
+                    active_dinosaurs.remove(Dinosaur)
+
+        if len(defeated_dinosaurs) >= 0:
+            for Dinosaur in defeated_dinosaurs:
+                if Dinosaur.health > 0:
+                    defeated_dinosaurs.remove(Dinosaur)
+                    active_dinosaurs.append(Dinosaur)
+                #Using the active dinosaur list defined when the player object is later created to determine which dinosaur name should be
+                #returned when the active dinosaur list is called.
+        if len(defeated_dinosaurs) == 1:
+            for Dinosaur in defeated_dinosaurs:
+                if Dinosaur.species == "t-rex":
+                    if Dinosaur.name == trex_name:
+                        print("Your defeated dinosaur is {dinosaur}.".format(dinosaur = trex_name))
+                    elif Dinosaur.name == trex2_name:
+                        print("Your defeated dinosaur is {dinosaur}.".format(dinosaur = trex2_name))
+                else:
+                    for Dinosaur in defeated_dinosaurs:
+                        if Dinosaur.name == stego_name:
+                            print("Your defeated dinosaur is {dinosaur}.".format(dinosaur = stego_name))
+                        if Dinosaur.name == stego2_name:
+                            print("Your defeated dinosaur is {dinosaur}.".format(dinosaur = stego2_name))
+        if len(defeated_dinosaurs) == 2:
+            if Dinosaur.species == "t-rex":
+                print("Your defeated dinosaurs are {dinosaur1} and {dinosaur2}. You currently have no active dinosaurs to duel with.".format(dinosaur1 = trex_name, dinosaur2 = trex2_name))
+            else:
+                print("Your defeated dinosaurs are {dinosaur1} and {dinosaur2}. You currently have no active dinosaurs to duel with.".format(dinosaur1 = stego_name, dinosaur2 = stego2_name))
+        elif len(defeated_dinosaurs) == 0:
+            print("You have no defeated dinosaurs.")
+
     def revive_dinosaur(self):
         pass
 
     def __repr__(self):
         return super().__repr__()
 
-#Creating the dinosaurs that will duel
-trex1 = Dinosaur(100, 20, 600, "t-rex")
-stegosaurus1 = Dinosaur(40, 60, 1000, "stegosarus")
-trex2 = Dinosaur(150, 70, 650, "t-rex")
-stegosaurus2 = Dinosaur(90, 110, 1050, "stegosarus")
+#The start of what will display in the terminal.
 
-#Creating player list of active dinosaurs
+#Global variables that will contain the names of the dinosaurs designated by the player.
+trex_name = input("Welcome to Dino Duel, Player 1. Player 1 you are the two T-rexes. Player 1, what would you like the name of your first t-rex to be? ").title()
+trex2_name = input("Player 1, what would you like the name of your second t-rex to be? ").title()
+stego_name = input("Welcome to Dino Duel, Player 2. Player 2 you are the Stegosaruses. Player 2, what would you like the name of your first stegosaurus to be? ").title()
+stego2_name = input("Player 2, what would you like the name of your second stegosaurus to be? ").title()
+
+#Creating the dinosaur objects that will duel.
+trex1 = Dinosaur(100, 20, 600, trex_name, "t-rex")
+stegosaurus1 = Dinosaur(40, 60, 1000, stego_name, "stegosarus")
+trex2 = Dinosaur(150, 70, 650, trex2_name, "t-rex")
+stegosaurus2 = Dinosaur(90, 110, 1050, stego2_name, "stegosarus")
+
+#Creating player list of active dinosaurs.
 player_one_dino_list = [trex1, trex2]
 player_two_dino_list = [stegosaurus1, stegosaurus2]
 
-#Creating player list of inactive, defeated dinosaurs
+#Creating player list of inactive, defeated dinosaurs.
 player_one_defeated_dinos = []
 player_two_defeated_dinos = []
 
-#Creating the two players
-player_one = Player(player_one_dino_list, player_one_defeated_dinos, "Player 1")
-player_two = Player(player_two_dino_list, player_two_defeated_dinos, "Player 2")
+#Creating the two player objects.
+player_one = Player(player_one_dino_list, player_one_defeated_dinos, 1)
+player_two = Player(player_two_dino_list, player_two_defeated_dinos, 1)
 
-#The start of what will display in the terminal
-player_one.name_dinosaurs(trex1, trex2)
-player_two.name_dinosaurs(stegosaurus1, stegosaurus2)
+print(cowsay.trex("The first T-rex is named " + trex_name))
+print(cowsay.trex("The second T-rex is named " + trex2_name))
+print(cowsay.stegosaurus("The first Stegosarus is named " + stego_name))
+print(cowsay.stegosaurus("The second Stegosarus is named " + stego2_name))
 
-#player_one.check_active_dinosaurs(player_one_dino_list)
+player_one.check_defeated_dinosaurs(player_one_defeated_dinos, player_one_dino_list)
+player_one.check_active_dinosaurs(player_one_dino_list, player_one_defeated_dinos)
+
+player_two.check_active_dinosaurs(player_two_dino_list, player_two_defeated_dinos)
+player_two.check_defeated_dinosaurs(player_two_defeated_dinos, player_two_dino_list)
+
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+trex2.lose_health(stegosaurus1.deal_damage())
+
+player_one.check_defeated_dinosaurs(player_one_defeated_dinos, player_one_dino_list)
+player_one.check_active_dinosaurs(player_one_dino_list, player_one_defeated_dinos)
+
+trex1.lose_health(stegosaurus1.deal_damage())
+
+player_one.check_defeated_dinosaurs(player_one_defeated_dinos, player_one_dino_list)
+player_one.check_active_dinosaurs(player_one_dino_list, player_one_defeated_dinos)
+
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+stegosaurus1.lose_health(trex1.deal_damage())
+
+player_two.check_active_dinosaurs(player_two_dino_list, player_two_defeated_dinos)
 
 stegosaurus1.lose_health(trex1.deal_damage())
 stegosaurus1.lose_health(trex1.deal_damage())
 stegosaurus1.lose_health(trex1.deal_damage())
 
+player_two.check_defeated_dinosaurs(player_two_defeated_dinos, player_two_dino_list)
+player_two.check_active_dinosaurs(player_two_dino_list, player_two_defeated_dinos)
+player_two.check_defeated_dinosaurs(player_two_defeated_dinos, player_two_dino_list)
+
+player_one.check_defeated_dinosaurs(player_one_defeated_dinos, player_one_dino_list)
+
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
+trex1.lose_health(stegosaurus1.deal_damage())
 trex1.lose_health(stegosaurus1.deal_damage())
 trex1.lose_health(stegosaurus1.deal_damage())
 trex1.lose_health(stegosaurus1.deal_damage())
 
-trex1.heal_self()
-stegosaurus1.heal_self()
+player_two.check_defeated_dinosaurs(player_two_defeated_dinos, player_two_dino_list)
 
-print(trex1)
-print(stegosaurus1)
-print(trex2)
-print(stegosaurus2)
+player_one.check_defeated_dinosaurs(player_one_defeated_dinos, player_one_dino_list)
+
+player_one.check_active_dinosaurs(player_one_dino_list, player_one_defeated_dinos)
+
+# trex1.heal_self()
+# stegosaurus1.heal_self()
+
+# print(trex1)
+# print(stegosaurus1)
+# print(trex2)
+# print(stegosaurus2)
